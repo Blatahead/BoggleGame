155e12aad1c93b288cecdbd92b59ea802766cdde
Blatahead <81589270+Blatahead@users.noreply.github.com>
2024-11-25 11:02:21 +0100

    form partie + class Partie

--- BoggleGameWinForm/Program.cs
+++ BoggleGameWinForm/Program.cs
@@ -13,20 +13,5 @@ namespace BoggleGame
             ApplicationConfiguration.Initialize();
             Application.Run(new Accueil());        
         }
-        /// <summary>
-        /// Méthode de création de plateau
-        /// </summary>
-        /// <param name="taille_plateau">taille du plateau renseigné par l'utilisateur</param>
-
-        static int[,] Creation_Plateau(int taille_plateau)
-        {
-            int[,] mat = null;
-            if (taille_plateau > 0)
-            {
-                mat = new int[taille_plateau, taille_plateau];
-            }
-            return mat;
-
-        }
     }
 }
