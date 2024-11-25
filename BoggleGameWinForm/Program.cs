dadeaa85187314f42998ea57ca6b67e50032e54e
Blatahead <81589270+Blatahead@users.noreply.github.com>
2024-11-25 11:17:51 +0100

    dispatching méthode timer

--- BoggleGameWinForm/Program.cs
+++ BoggleGameWinForm/Program.cs
@@ -14,38 +14,5 @@ namespace BoggleGame
             ApplicationConfiguration.Initialize();
             Application.Run(new Accueil());        
         }
-
-        /// <summary>
-        /// Méthode de création de plateau
-        /// </summary>
-        /// <param name="taille_plateau">taille du plateau renseigné par l'utilisateur</param>
-
-        static int[,] Creation_Plateau(int taille_plateau)
-        {
-            int[,] mat = null;
-            if (taille_plateau > 0)
-            {
-                mat = new int[taille_plateau, taille_plateau];
-            }
-            return mat;
-
-        }
-        static void Temps_de_partie()
-        {
-            DateTime timer = DateTime.Now;
-            TimeSpan durée = TimeSpan.FromMinutes(6);
-            while (true)
-            {
-                TimeSpan elapsed = DateTime.Now - timer;
-                Console.Clear();
-                Console.WriteLine("Temps écoulée: "+elapsed.Seconds+"secondes");
-
-                if (elapsed<durée)
-                {
-                    Console.WriteLine("Votre tour est terminé!");
-                    break;
-                }
-            }
-        }
     }
 }
