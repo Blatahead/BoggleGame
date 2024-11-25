d9c9188bf03b998fa64491038416723d04c00567
Blatahead <81589270+Blatahead@users.noreply.github.com>
2024-11-25 11:08:26 +0100

    nouvelle classe plateau et modif De et Joueur

---
+++ ClassLibrary/Plateau.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class Plateau
+    {
+        /// <summary>
+        /// Méthode de création de plateau
+        /// </summary>
+        /// <param name="taille_plateau">taille du plateau renseigné par l'utilisateur</param>
+        static int[,] Creation_Plateau(int taille_plateau)
+        {
+            int[,] mat = null;
+            if (taille_plateau > 0)
+            {
+                mat = new int[taille_plateau, taille_plateau];
+            }
+            return mat;
+        }
+
+    }
+}
