234d5e94ff46322ffa4b4f6e8101d5e84101805c
Mathieu ANTUNES--HOLUÉ <mathieu.antunes--holue@edu.devinci.fr>
2024-12-13 16:44:29 +0100

    ajout méthode toString dico+ remplissage de la SortedList1

--- ClassLibrary/Joueur.cs
+++ ClassLibrary/Joueur.cs
@@ -47,26 +47,7 @@ namespace ClassLibrary
 
         #region Méthodes
 
-        /// <summary>
-        /// Fonction qui lance un chrono pour chaque joueur durant son tour
-        /// </summary>
-        public void TimerJoueur()
-        {
-            DateTime timer = DateTime.Now;
-            TimeSpan durée = TimeSpan.FromMinutes(1);
-            while (true)
-            {
-                TimeSpan elapsed = DateTime.Now - timer;
-                Console.Clear();
-                Console.WriteLine("Temps écoulée: " + elapsed.Seconds + "secondes");
-
-                if (elapsed < durée)
-                {
-                    Console.WriteLine("Votre tour est terminé!");
-                    break;
-                }
-            }
-        }
+        
 
         #endregion
     }
