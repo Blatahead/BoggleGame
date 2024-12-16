c3148f50fce8fb176ecd4a38a2d442659aafc3b6
Antonin <81589270+Blatahead@users.noreply.github.com>
2024-12-16 09:41:39 +0100

    accueil clean

--- BoggleGameWinForm/Accueil.cs
+++ BoggleGameWinForm/Accueil.cs
@@ -9,26 +9,20 @@ namespace BoggleGame
 {
     public partial class Accueil : Form
     {
-        private Image imageFond;
-
+        #region Constructeurs
         public Accueil()
         {
             InitializeComponent();
             CustomizeButtons();
 
-            // Afficher un délai avant de charger l'image
+            // Délai avant de charger l'image
             this.Load += async (s, e) =>
             {
                 try
                 {
-                    // Simuler un délai (optionnel)
                     await Task.Delay(250);
 
-                    // Charger l'image
-                    imageFond = Image.FromFile("./../../../../background.jpg");
-
-                    // Appliquer l'image comme fond
-                    this.BackgroundImage = imageFond;
+                    this.BackgroundImage = Image.FromFile("./../../../../background.jpg");
                     this.BackgroundImageLayout = ImageLayout.Stretch;
                 }
                 catch (Exception ex)
@@ -37,66 +31,69 @@ namespace BoggleGame
                 }
             };
         }
+        #endregion
 
-
-
-
+        #region Méthodes
+        /// <summary>
+        /// Design de tous les boutons de l'accueil
+        /// </summary>
         private void CustomizeButtons()
         {
-            // Configurer le bouton "Nouvelle partie"
             ConfigureButton(newGameBtn);
-
-            // Configurer le bouton "Règles"
             ConfigureButton(buttonRules);
 
-            // Ajuster la taille et la position des boutons dynamiquement
+            // Responsive
             this.Resize += (s, e) =>
             {
-                // Bouton "Nouvelle partie" : centré en haut
                 PositionButton(newGameBtn, 2, -100);
-
-                // Bouton "Règles" : centré en bas
                 PositionButton(buttonRules, 2, 100);
             };
         }
 
+        /// <summary>
+        /// Design d'un bouton
+        /// </summary>
+        /// <param name="button"></param>
         private void ConfigureButton(Button button)
         {
-            button.BackColor = Color.FromArgb(128, 45, 156, 219); // Bleu doux avec opacité
-            button.ForeColor = Color.White; // Texte en blanc
+            button.BackColor = Color.FromArgb(128, 45, 156, 219);
+            button.ForeColor = Color.White;
             button.Font = new Font("Young Serif", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button.FlatStyle = FlatStyle.Flat;
             button.FlatAppearance.BorderSize = 0;
 
-            // Coins arrondis
             button.Paint += Button_Paint;
         }
 
+        /// <summary>
+        /// Méthode qui arrondi les coins d'un bouton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Paint(object sender, PaintEventArgs e)
         {
             Button btn = (Button)sender;
 
-            // Créer un pinceau pour les coins arrondis
+            // Coins arrondis
             using (GraphicsPath path = new GraphicsPath())
             {
-                path.AddArc(0, 0, 20, 20, 180, 90); // Coin supérieur gauche
-                path.AddArc(btn.Width - 20, 0, 20, 20, 270, 90); // Coin supérieur droit
-                path.AddArc(btn.Width - 20, btn.Height - 20, 20, 20, 0, 90); // Coin inférieur droit
-                path.AddArc(0, btn.Height - 20, 20, 20, 90, 90); // Coin inférieur gauche
+                path.AddArc(0, 0, 20, 20, 180, 90); // Haut gauche
+                path.AddArc(btn.Width - 20, 0, 20, 20, 270, 90); // Haut droit
+                path.AddArc(btn.Width - 20, btn.Height - 20, 20, 20, 0, 90); // Bas droit
+                path.AddArc(0, btn.Height - 20, 20, 20, 90, 90); // Bas gauche
                 path.CloseAllFigures();
 
-                // Appliquer la région arrondie
                 btn.Region = new Region(path);
-
-                // Dessiner un contour
-                using (Pen pen = new Pen(Color.FromArgb(35, 135, 200), 2))
-                {
-                    e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
-                    e.Graphics.DrawPath(pen, path);
-                }
             }
         }
 
+        /// <summary>
+        /// Méthode qui gère la position et la taille d'un bouton.
+        /// Sert au responsive d'un bouton.
+        /// </summary>
+        /// <param name="button"></param>
+        /// <param name="widthDivider"></param>
+        /// <param name="verticalOffset"></param>
         private void PositionButton(Button button, int widthDivider, int verticalOffset)
         {
             int largeur = this.ClientSize.Width / widthDivider; // Fraction de largeur disponible
@@ -108,24 +105,27 @@ namespace BoggleGame
             button.Location = new Point(posX, posY);
         }
 
-        private void Chargement_Accueil(object sender, EventArgs e)
-        {
-            int hauteur = Screen.PrimaryScreen.Bounds.Height;
-            int largeur = Screen.PrimaryScreen.Bounds.Width;
-            this.Location = new Point(0, 0);
-            this.Size = new Size(largeur, hauteur);
-        }
-
+        /// <summary>
+        /// Lance la partie si bouton cliqué
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ButtonGameClick(object sender, EventArgs e)
         {
             Partie partie = new Partie();
             partie.Show();
         }
 
+        /// <summary>
+        /// Affiche la page de règles si bouton cliqué
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonRulesClick(object sender, EventArgs e)
         {
             Regles regles = new Regles();
             regles.Show();
         }
+        #endregion
     }
 }
